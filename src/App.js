f493e7d14b12baee224817665f2ebfb281b9b34b
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-02-19 21:52:25 +0530

    added Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styles.css";
 import Header from './Header';
 import MemeGenerator from './MemeGenerator';
+import Footer from './Footer';
 
 export default function App() {
   return (
     <div className="App">
       <Header />
       <MemeGenerator />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.css';
+
+export default function Footer() {
+  return (
+    <p className="App footer">Copyright &copy; 2021 Vinod Kumar R</p>
+  )
+}
